6aa9f1b5853c21d392cefc66e39afbbc1e46b417
xumoumou1 <55374440+xumoumou1@users.noreply.github.com>
2023-07-20 18:05:32 +0800

    Add files via upload

---
+++ test/src/settings.2d95b.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/Login.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "4210e",
+        main: "9004f"
+    }
+};
