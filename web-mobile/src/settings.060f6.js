5ef1246640ca74fa235c9d35af35f5c61e58b3db
xumoumou1 <55374440+xumoumou1@users.noreply.github.com>
2023-08-07 16:10:43 +0800

    Delete web-mobile directory

--- web-mobile/src/settings.060f6.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d17e2",resources:"4d696",Test:"2a22f",TestManual:"482b2",main:"58743"}};
