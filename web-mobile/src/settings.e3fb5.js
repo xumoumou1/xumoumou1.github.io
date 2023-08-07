0cc26110158388fe0820074a7bb23ea7124fc526
xumoumou1 <55374440+xumoumou1@users.noreply.github.com>
2023-08-07 15:54:19 +0800

    Add files via upload

---
+++ web-mobile/src/settings.e3fb5.js
@@ -0,0 +1,31 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "magnifying"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/helloworld.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        resources: "76216",
+        Test: "d45c3",
+        TestManual: "de88e",
+        main: "f1453"
+    }
+};
